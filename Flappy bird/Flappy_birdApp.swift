c28197e35574db5ba7c6e2bfd3cc25e66db55083
KabirSingh <kabirsingh07@gmail.com>
2024-06-03 09:39:04 -0700

    Initial Commit

---
+++ Flappy bird/Flappy_birdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_birdApp.swift
+//  Flappy bird
+//
+//  Created by Kabir on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Flappy_birdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
